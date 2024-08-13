db2e93da61262b74b2f21764913cbd3d536724e6
Robin Gloster <mail@glob.in>
2024-08-13 19:30:34 +0200

    messages: clone for AtcPositionUpdateMessage

--- src/messages.rs
+++ src/messages.rs
@@ -237,7 +237,7 @@ impl PilotDeregisterMessage {
 }
 
 /// Sent at regular intervals by an ATC client to update the server with its position
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct AtcPositionUpdateMessage {
     pub callsign: String,
     pub frequencies: Vec<RadioFrequency>,
